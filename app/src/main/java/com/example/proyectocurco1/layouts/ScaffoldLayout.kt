fef631027daf613315f07c89bcf758f41b7ec223
UlisesVeraC <veracuevasu@gmail.com>
2024-12-26 15:07:39 -0600

    TopBar

--- app/src/main/java/com/example/proyectocurco1/layouts/ScaffoldLayout.kt
+++ app/src/main/java/com/example/proyectocurco1/layouts/ScaffoldLayout.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,9 +20,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldLayout(){
     Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Magenta
+                ),
+                title = {
+                    Text(text="AppBar")
+            })
+        },
         floatingActionButton = {
             FloatingActionButton(
                 containerColor = Color.Gray,
