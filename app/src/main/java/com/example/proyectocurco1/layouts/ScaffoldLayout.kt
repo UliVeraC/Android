db5c192da27be0296776ed493c6a451558f999a4
UlisesVeraC <veracuevasu@gmail.com>
2024-12-26 20:01:28 -0600

    Boxes, columns, scaffold, etc

--- app/src/main/java/com/example/proyectocurco1/layouts/ScaffoldLayout.kt
+++ app/src/main/java/com/example/proyectocurco1/layouts/ScaffoldLayout.kt
@@ -1,23 +1,19 @@
 package com.example.proyectocurco1.layouts
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,7 +23,7 @@ fun ScaffoldLayout(){
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Red
+                    containerColor = Color.Gray
                 ),
                 title = {
                     Text(text="AppBar")
@@ -47,11 +43,14 @@ fun ScaffoldLayout(){
         }
     ) { paddingValues ->
         Box (
-            contentAlignment = Alignment.BottomEnd,
             modifier = Modifier.padding(paddingValues)
                 .fillMaxSize()
         ){
-
+            Column {
+                ItemContact(name = "Ulises", phoneNumber = "7714144641")
+                ItemContact(name = "Fernando", phoneNumber = "7751234567")
+                ItemContact(name = "Pedro", phoneNumber = "7756456875")
+            }
         }
     }
 }
